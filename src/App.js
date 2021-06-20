9996dbc4f2cb341fe2e03b0472fc769bcae07876
Matvei Bohush <bohush.mathew@gmail.com>
2021-06-20 11:45:10 +0300

    add shop page and styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css'
 import Homepage from './pages/homepage/homepage'
 import { Switch, Route } from 'react-router-dom'
+import ShopPage from './pages/shop/shop'
 
 function App() {
     return (
         <div>
             <Switch>
                 <Route exact path="/" component={Homepage} />
+                <Route path="/shop" component={ShopPage} />
             </Switch>
         </div>
     )
 }
 
 export default App
+ 
