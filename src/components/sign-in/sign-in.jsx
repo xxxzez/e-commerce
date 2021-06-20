74377cdfe72febac4e98382cc7aed3b49f674060
Matvei Bohush <bohush.mathew@gmail.com>
2021-06-20 16:33:39 +0300

    add sign in and sign out bottons and styles

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -42,10 +42,12 @@ class SignIn extends Component {
                         required
                         onChange={this.handleChange}
                     />
-                    <CustomButton type="submit">Sign in</CustomButton>
-                    <CustomButton onClick={signInWithGoogle}>
-                        Sign in with Google
-                    </CustomButton>
+                    <div className="buttons">
+                        <CustomButton type="submit">Sign in</CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                            Sign in with Google
+                        </CustomButton>
+                    </div>
                 </form>
             </div>
         )
