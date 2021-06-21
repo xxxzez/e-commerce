5859d1e36afe45f1a5d43ecaa1baa9e85dcd8811
Matvei Bohush <bohush.mathew@gmail.com>
2021-06-21 15:19:25 +0300

    add sign-up component

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -1,9 +1,11 @@
 import SignIn from '../../components/sign-in/sign-in'
+import SignUp from '../../components/sign-up/sign-up'
 import './sign-in-and-sign-up.styles.scss'
 
 const SignInAndSignUp = () => (
     <div className="sign-in-and-sign-up">
         <SignIn />
+        <SignUp />
     </div>
 )
 
