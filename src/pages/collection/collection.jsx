0213aec7a4e6e06e50b8d42680b605d1f151a350
Matvei Bohush <bohush.mathew@gmail.com>
2021-06-25 22:50:23 +0300

    add selectCollection selector

--- src/pages/collection/collection.jsx
+++ src/pages/collection/collection.jsx
@@ -1,8 +1,15 @@
+import { connect } from 'react-redux'
+import { selectCollection } from '../../redux/shop/shop.selectors'
 import './collection.styles.scss'
 
-const CollectionPage = ({match}) => (
+const CollectionPage = ({ match }) => (
     <div className="collection-page">
         <h2>Category Page</h2>
     </div>
 )
-export default CollectionPage
+
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state),
+})
+
+export default connect(mapStateToProps)(CollectionPage)
