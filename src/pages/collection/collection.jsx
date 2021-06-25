12d71f6b5fb9a8ca9be12eab15428aa66035c822
Matvei Bohush <bohush.mathew@gmail.com>
2021-06-25 22:19:18 +0300

    add collection-overview

---
+++ src/pages/collection/collection.jsx
@@ -0,0 +1,8 @@
+import './collection.styles.scss'
+
+const CollectionPage = ({match}) => (
+    <div className="collection-page">
+        <h2>Category Page</h2>
+    </div>
+)
+export default CollectionPage
