4080b60dd0aa96ae8b7be1550b25bb21f612c70e
Matvei Bohush <bohush.mathew@gmail.com>
2021-06-27 12:35:31 +0300

    add with-spinner

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,38 +1,59 @@
-import { Component } from 'react'
 import { Route } from 'react-router-dom'
-import { firestore } from '../../firebase/firebase.utils'
-import CollectionsOverview from '../../components/collections-overview/collections-overview'
-import { convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils'
-import CollectionPage from '../collection/collection'
-import { UpdateCollections } from '../../redux/shop/shop.actions'
 import { connect } from 'react-redux'
+import {
+    firestore,
+    convertCollectionsSnapshotToMap,
+} from '../../firebase/firebase.utils.js'
+import { Component } from 'react'
+import { UpdateCollections } from '../../redux/shop/shop.actions'
+import WithSpinner from '../../components/with-spinner/with-spinner'
+import CollectionsOverview from '../../components/collections-overview/collections-overview'
+import CollectionPage from '../collection/collection'
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview)
+const CollectionPageWithSpinner = WithSpinner(CollectionPage)
 
 class ShopPage extends Component {
+    state = {
+        loading: true,
+    }
+
     unsubscribeFromSnapshot = null
+
     componentDidMount() {
         const { updateCollections } = this.props
         const collectionRef = firestore.collection('collections')
 
-        this.unsubscribeFromSnapshot = collectionRef.onSnapshot(
-            async (snapshot) => {
-                const collectionsMap = convertCollectionsSnapshotToMap()
-                updateCollections(collectionsMap)
-            }
-        )
+        collectionRef.get().then((snapshot) => {
+            const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
+            updateCollections(collectionsMap)
+            this.setState({ loading: false })
+        })
     }
 
     render() {
         const { match } = this.props
+        const { loading } = this.state
         return (
             <div className="shop-page">
                 <Route
                     exact
                     path={`${match.path}`}
-                    component={CollectionsOverview}
+                    render={(props) => (
+                        <CollectionsOverviewWithSpinner
+                            isLoading={loading}
+                            {...props}
+                        />
+                    )}
                 />
                 <Route
                     path={`${match.path}/:collectionId`}
-                    component={CollectionPage}
+                    render={(props) => (
+                        <CollectionPageWithSpinner
+                            isLoading={loading}
+                            {...props}
+                        />
+                    )}
                 />
             </div>
         )
--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,7 +1,7 @@
 import { ShopActionTypes } from './shop.types'
 
 const INITIAL_STATE = {
-    collections: SHOP_DATA,
+    collections: null ,
 }
 
 const shopReducer = (state = INITIAL_STATE, action) => {
